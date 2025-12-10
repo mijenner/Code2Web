73bb0cf780614a80db4a613c9e08ea7864d2d8f2
mijenner <mijenner@gmail.com>
2025-12-10 16:14:24 +0100

    Add project files.

---
+++ cliCode2Web/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cliCode2Web
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
